49ff7a9da9632f6341c55e52991438e4715aeea0
alarwasyi98 <alarwasyi98@gmail.com>
2024-06-20 17:37:30 +0700

    Changing Terminal Icon

--- .pwsh/Microsoft.PowerShell_Profile.ps1
+++ .pwsh/Microsoft.PowerShell_Profile.ps1
@@ -1,4 +1,4 @@
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
-$ENV:STARSHIP_DISTRO = "  alarwasyi98 "
+$ENV:STARSHIP_DISTRO = "  alarwasyi98 "
 $ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
 Invoke-Expression (&starship init powershell)
