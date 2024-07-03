f3c4c1efbff58b6e4fc3b72033bd6e6e424e0de8
alarwasyi98 <alarwasyi98@gmail.com>
2024-07-04 02:54:16 +0700

    frustated: still cant recall aliases, modules and script via $PROFILE

--- PowerShell/Microsoft.PowerShell_Profile.ps1
+++ PowerShell/Microsoft.PowerShell_Profile.ps1
@@ -1,44 +1,42 @@
-$ScriptsPath = Join-Path $PSScriptRoot "Scripts"
-
-# Load Aliases
-. (Join-Path $ScriptsPath "Aliases.ps1")
-
-# Load Functions
-. (Join-Path $ScriptsPath "Functions.ps1")
-
-# Load Modules
-. (Join-Path $ScriptsPath "Modules.ps1")
-
-# Starship Prompt Conf
-$ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
-$ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
-$ENV:STARSHIP_DISTRO = "  alarwasyi98 "
-
 # ChocolateyProfile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
 
+# Starship Prompt Conf
+# $ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
+# $ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
+$ENV:STARSHIP_DISTRO = "  alarwasyi98 "
 
+# Aliases
+Set-Alias tt tree
+# Set-Alias ll ls
+Set-Alias vim nvim
+Set-Alias cat bat
+
+##########################
 # Linux-Like Function
+##########################
 
 # System Utilities
 function admin {
     if ($args.Count -gt 0) {
         $argList = "& '$args'"
         Start-Process wt -Verb runAs -ArgumentList "pwsh.exe -NoExit -Command $argList"
-    } else {
+    }
+    else {
         Start-Process wt -Verb runAs
     }
 }
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command with elevated rights.
+Set-Alias -Name su -Value admin
 
-
-
+# Easy Navigate Program
 function whereis ($command) {
-	Get-Command -Name $command -ErrorAction SilentlyContinue |
-	Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-	}
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
 
 function touch($file) { "" | Out-File $file -Encoding ASCII }
 function ff($name) {
@@ -62,10 +60,49 @@ function dev { Set-Location -Path x:\ }
 function ep { vim $PROFILE }
 function staredit {
     param (
-        [string]$FilePath = "C:\Users\alarw\.config\starship.toml"
+        [string]$FilePath = "C:\Users\alarwasyi98\.config\starship.toml"
     )
     nvim $FilePath
 }
+
+# Open Windows Utilitie Chris Titus
+function winutil {
+	Invoke-WebRequest -useb https://christitus.com/win | Invoke-Expression
+}
+
+# Enhanced Listing
+function la { Get-ChildItem -Path . -Force | Format-Table -AutoSize }
+function ln { Get-ChildItem -Name | Format-Table -Autosize}
+
+# Git Shortcuts
+function gs { git status }
+function ga { git add . }
+function gc { param($m) git commit -m "$m" }
+function gp { git push }
+function g { z Github }
+function gcl { git clone "$args" }
+function gcom {
+    git add .
+    git commit -m "$args"
+}
+function lazyg {
+    git add .
+    git commit -m "$args"
+    git push
+}
+
+# Modules Importer
+# Terminal-Icons
+Import-Module Terminal-Icons
+
+# PSReadline
+Import-Module PSReadline
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadlineOption -PredictionViewStyle ListView
+Set-PsReadlineOption -PredictionSource History
+
+# PSGallery => PowerShellGet Package Manager Credential
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 
 # Invoke Expressions
 Invoke-Expression (&starship init powershell)
