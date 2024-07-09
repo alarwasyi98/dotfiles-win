1ef9d41dfe6569dd3fcdc27ecb4c3cc22c758344
alarwasyi98 <alarwasyi98@gmail.com>
2024-07-10 01:15:37 +0700

    fix: fonts script

---
+++ Fonts/fonts.ps1
@@ -0,0 +1,5 @@
+# This script just can to be run after Scoop Installed
+scoop bucket add nerd-fonts
+scoop install nerd-fonts/FiraCode-NF
+scoop install nerd-fonts/Hack-NF
+echo "Fonts Installed Succesfully"
