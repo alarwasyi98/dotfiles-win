6fc4bdac51b9284f7fb069f0c65c0ced7da03937
alarwasyi98 <alarwasyi98@gmail.com>
2024-07-20 17:24:43 +0700

    ci: initialize scripts

---
+++ Scripts/Chocolatey.ps1
@@ -0,0 +1,7 @@
+# Choco install
+try {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+catch {
+    Write-Error "Failed to install Chocolatey. Error: $_"
+}
