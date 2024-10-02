26c39a6d861cec89ed54f43a40656210b19ab8e1
Abdul Hakim <alarwasyi98@gmail.com>
2024-10-02 22:51:43 +0700

    remove ./scripts/ dir

--- Windows/PowerShell/Scripts/Aliases.ps1
+++
@@ -1,8 +0,0 @@
-# Aliases
-Set-Alias tt tree
-# Set-Alias ll ls
-Set-Alias vim nvim
-Set-Alias cat bat
-
-# Set UNIX-like aliases for the admin command, so sudo <command> will run the command with elevated rights.
-Set-Alias -Name su -Value admin
--- Windows/PowerShell/Scripts/Modules.ps1
+++
@@ -1,9 +0,0 @@
-# Modules Importer
-# Terminal-Icons
-Import-Module Terminal-Icons
-
-# PSReadline
-Import-Module PSReadline
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Set-PSReadlineOption -PredictionViewStyle ListView
-Set-PsReadlineOption -PredictionSource History
